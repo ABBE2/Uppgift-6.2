92c7adf25477c1d22235756f3d6bc93e3ee70847
Admin <Admin@TEB127030>
2023-05-12 09:02:37 +0200

    Add project files.

---
+++ Uppgift 6.2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace program
+{
+    class program
+    {
+        static int Störst(int tal1, int tal2)
+        {
+            if(tal1 < tal2)
+            {
+                return tal2;
+            }
+            else if (tal1 > tal2)
+            {
+                return tal1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv två heltal");
+            int tal1 = Convert.ToInt32(Console.ReadLine());
+            int tal2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.Write("Här ser vi att ");
+            if (tal1 != tal2)
+            {
+                int tal3 = Störst(tal1, tal2);
+                Console.WriteLine(tal3 + " är störst");
+            }
+            else
+            {
+                Console.WriteLine("talen har samma värde.");
+            }
+        }
+    }
+}
